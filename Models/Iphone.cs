65625b810a96162c8bd9570def922623ae35b31a
walkiriatorres <85717908+walkiriatorres@users.noreply.github.com>
2023-12-19 04:25:39 -0300

    Update Iphone.cs
    Adicionando propriedades, construtores, métodos Get e sobrescrevendo InstalarAplicativo.

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,45 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        private double MemoriaDisponivel;      
+        private List<string> AplicativosInstalados;
+
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            MemoriaDisponivel = memoria;
+            AplicativosInstalados = new List<string>();
+        }
+
+        public double GetMemoriaDisponivel()
+        {
+            return MemoriaDisponivel;
+        }
+
+        public List<string> GetAplicativosInstalados()
+        {
+            return AplicativosInstalados;
+        }
+
+        public override void InstalarAplicativo(string nomeApp, int tamanho)
+        {
+            try
+            {
+                if (MemoriaDisponivel >= tamanho) {
+                    Console.WriteLine($"Fazendo download do app {nomeApp} na AppStore...");
+                    MemoriaDisponivel -= tamanho;
+                    AplicativosInstalados.Add(nomeApp);
+                    Console.WriteLine($"Download realizado com sucesso. O app {nomeApp} está disponível para uso.");
+                } else {
+                    double espacoNecessario = tamanho - MemoriaDisponivel;
+                    Console.Write($"Não há memória disponível suficiente. Libere {espacoNecessario} GB para realizar o download do {nomeApp}.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write($"Ocorreu um erro ao instalar aplicativo. {ex.Message}");
+            }
+        }
     }
 }
