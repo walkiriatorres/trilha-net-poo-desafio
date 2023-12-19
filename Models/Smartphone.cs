b8a2d8e4f2a65bb3c7215d71739583178d55c35f
walkiriatorres <85717908+walkiriatorres@users.noreply.github.com>
2023-12-19 04:13:09 -0300

    Update Smartphone.cs
    Inserindo propriedades private, contrutor, métodos Get e atualizando método Ligar.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,24 +3,48 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo;
+        private string Imei;
+        private double Memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
-
-        public void Ligar()
+        public double GetMemoria()
         {
-            Console.WriteLine("Ligando...");
+            return Memoria;
         }
-
+        public string GetModelo()
+        {
+            return Modelo;
+        }
+        public string GetImei()
+        {
+            return Imei;
+        }
+        public void Ligar(string numero)
+        {
+            try
+            {
+                if (!Numero.Equals(numero)) {
+                Console.WriteLine($"Ligando para o número: {numero}...");
+                } else {
+                Console.WriteLine($"Não é possível ligar para o próprio número.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write($"Ocorreu um erro ao realizar chamada. {ex.Message}");
+            }            
+        }
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
-
-        public abstract void InstalarAplicativo(string nomeApp);
+        public abstract void InstalarAplicativo(string nomeApp, int tamanho);
     }
 }
